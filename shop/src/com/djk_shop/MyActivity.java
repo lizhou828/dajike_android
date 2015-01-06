8eb5a40d0c29e60c65ebb278be5e4ce49728a2a8
lizhou828 <lizhou828@126.com>
2015-01-06 13:23:31 +0800

    add quit alert dialog in MyActivity of shop module

--- shop/src/com/djk_shop/MyActivity.java
+++ shop/src/com/djk_shop/MyActivity.java
@@ -1,13 +1,31 @@
 package com.djk_shop;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
 public class MyActivity extends Activity implements View.OnClickListener{
+
     private Button loginButton;
+
+    //Listener of back button
+    private DialogInterface.OnClickListener backButtonListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int which) {
+            switch (which){
+                case AlertDialog.BUTTON_POSITIVE :finish();
+                    break;
+                case AlertDialog.BUTTON_NEGATIVE :
+                    break;
+                default:break;
+            }
+        }
+    };
 
     /**
      * Called when the activity is first created.
@@ -31,6 +49,22 @@ public class MyActivity extends Activity implements View.OnClickListener{
                 startActivity( new Intent(MyActivity.this,LoginActivity.class));
                 break;
             default:break;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //if current user pressed the back button
+        if( keyCode == KeyEvent.KEYCODE_BACK ){
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("系统提示:");
+            alertDialog.setMessage("您确定要退出吗?");
+            alertDialog.setButton("确定", backButtonListener);
+            alertDialog.setButton2("取消",backButtonListener);
+            alertDialog.show();
+            return false;
+        }else{
+            return super.onKeyDown(keyCode, event);
         }
     }
 }
